3f8d9e0926b34acadd9f0060f7d56af3b1b34276
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-25 20:17:47 -0300

    Initial commit

---
+++ src/main/java/br/com/runner/storecorner/StorecornerApplication.java
@@ -0,0 +1,13 @@
+package br.com.runner.storecorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorecornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorecornerApplication.class, args);
+	}
+
+}
