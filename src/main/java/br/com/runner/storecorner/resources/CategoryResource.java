bad637780e6747fbc57d85b16350d26a0b3da9ba
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-25 21:00:27 -0300

    Add Category

--- src/main/java/br/com/runner/storecorner/resources/CategoryResource.java
+++ src/main/java/br/com/runner/storecorner/resources/CategoryResource.java
@@ -1,15 +1,25 @@
 package br.com.runner.storecorner.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.runner.storecorner.domain.Category;
+
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST OK";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
