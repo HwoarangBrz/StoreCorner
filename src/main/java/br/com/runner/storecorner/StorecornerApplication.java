4ee6ba554e9cd8a394b24e2530f8c772f4d8e56b
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-26 20:58:34 -0300

    Instantiation

--- src/main/java/br/com/runner/storecorner/StorecornerApplication.java
+++ src/main/java/br/com/runner/storecorner/StorecornerApplication.java
@@ -1,13 +1,32 @@
 package br.com.runner.storecorner;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StorecornerApplication {
+import br.com.runner.storecorner.domain.Category;
+import br.com.runner.storecorner.repository.CategoryRepository;
 
+@SpringBootApplication
+public class StorecornerApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository repoCategory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StorecornerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Hardware");
+		Category cat2 = new Category(null, "Software");
+		
+		repoCategory.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
