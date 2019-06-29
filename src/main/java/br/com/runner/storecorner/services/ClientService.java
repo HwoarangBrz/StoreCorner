45bc6915a703a5fee92b857ee262d255be05a93b
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-29 15:09:43 -0300

    Add endpoint clients

---
+++ src/main/java/br/com/runner/storecorner/services/ClientService.java
@@ -0,0 +1,25 @@
+package br.com.runner.storecorner.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.runner.storecorner.domain.Category;
+import br.com.runner.storecorner.domain.Client;
+import br.com.runner.storecorner.repository.ClientRepository;
+import br.com.runner.storecorner.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repo;
+	
+	public Client find(Integer id) {
+		Optional<Client> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Client.class.getName()));
+	}
+	
+}
